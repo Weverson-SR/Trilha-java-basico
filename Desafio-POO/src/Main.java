f9c9a969de66aca0f1ec63b72fffc19624424157
Weverson <weversonwrs8@gmail.com>
2024-06-06 13:36:11 -0300

    Mentoria e Curso

---
+++ Desafio-POO/src/Main.java
@@ -0,0 +1,29 @@
+import java.time.LocalDate;
+
+import BrComDesafioDioDominio.Curso;
+import BrComDesafioDioDominio.Mentoria;
+
+public class Main {
+    public static void main(String[] args){
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("curso java");
+        curso1.setDescricao("Descrição curso");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("curso js");
+        curso2.setDescricao("Descrição curso js");
+        curso2.setCargaHoraria(4);
+
+        Mentoria mentoria = new Mentoria();
+
+        mentoria.setTitulo("Mentorioa de java");
+        mentoria.setDescricao("Descrição mentoria java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+    }
+}
